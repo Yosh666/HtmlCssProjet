9b6c2ca6061107136776ccacb0c4b158baa35b1a
Yosh666 <60882174+Yosh666@users.noreply.github.com>
2020-02-11 20:36:47 +0100

    html+css+php v1
    a finaliser sur css et faire le responsive en js

---
+++ cible.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Action super globale</title>
+</head>
+<body>
+    <p>Holà Saigneur et maître</p>
+    <p>
+        Nous clamerons que le grand <?php echo $_POST['firstname']; ?> est un <?php echo['lastname']; ?> de grande renommée!
+    </p>
+    <p>
+        Change ta race <a href="page1.html">ici</a> pour devenir plus puissant (et masculin aussi ça m'arrange)
+    </p>
+
+</body>
+</html>
